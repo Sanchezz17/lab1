2b0d2ae52f9ef2fedc80ceef0572fa3e9eb98dcf
Евгений <gekachan2000@gmail.com>
2019-10-11 20:58:22 +0500

    create wave

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -24,11 +24,12 @@ private:
     int B_OUT;
 };
 
-Led leds[5] = { Led(6, 7, 8), Led(9, 10, 11), Led(12, 13, 14), Led(15, 16, 17), Led(18, 19, 20) };
+//Led leds[5] = { Led(6, 7, 8), Led(9, 10, 11), Led(12, 13, 14), Led(15, 16, 17), Led(18, 19, 20) };
+Led leds[5] = { Led(6, 7, 8), Led(9, 10, 11), Led(31, 33, 35), Led(37, 39, 41), Led(43, 45, 47) };
 int currentLedNumber = 0;
 bool waveOn = false;
 
-void set_rgb_led(Led led, int R = 255, int G = 255, int B = 255)
+void set_rgb_led(Led led, int R = 0, int G = 0, int B = 0)
 {
     analogWrite(led.GetR_OUT(), 255 - R);
     analogWrite(led.GetG_OUT(), 255 - G);
@@ -43,36 +44,53 @@ void setup()
         pinMode(led.GetG_OUT(), OUTPUT);
         pinMode(led.GetB_OUT(), OUTPUT);
     }
+    offAllLeds();
 }
 
 void loop() 
 {
-    if (button.wasPressed())
-    {
-        waveOn = !waveOn;
-        if (!waveOn)
-        {
-            stopWave();
-        }
-    }
+    checkOff();
     if (waveOn)
     {
         waveIteration();  
     }
 }
 
-void waveIteration()
-{
-    set_rgb_led(leds[currentLedNumber], 0, 255, 0);
-    delay(500);
-    set_rgb_led(leds[currentLedNumber]);
-    currentLedNumber = (currentLedNumber + 1) % 5;
+bool checkOff() {
+    if (button.wasPressed())
+    {
+        waveOn = !waveOn;
+        if (!waveOn)
+        {
+            offAllLeds();
+            return true;
+        }
+    }
+    return false;
 }
 
-void stopWave()
+void waveIteration()
+{
+    Led currentLed = leds[currentLedNumber];
+    int nextLedNumber = (currentLedNumber + 1) % 5;
+    Led nextLed = leds[nextLedNumber];
+    for (int i = 255; i >= 0; i--)
+    {
+        if (checkOff())
+            return;
+        delay(20);
+        set_rgb_led(currentLed, i, i, i);
+        delay(20);
+        set_rgb_led(nextLed, 255 - i, 255 - i, 255 - i);
+    }
+
+    currentLedNumber = nextLedNumber;
+}
+
+void offAllLeds()
 {
     for (Led led: leds)
     {
-        set_rgb_led(leds[currentLedNumber]);
+        set_rgb_led(led);
     }
 }
