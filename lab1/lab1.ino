2f303d6ad43df51af89a7a8a6a697edbb55d927d
Евгений <gekachan2000@gmail.com>
2019-11-02 15:54:22 +0500

    Remove class Led

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,62 +1,31 @@
 #include <Arduino.h>
 #include "button.h"
-using namespace std;
 
 #define PIN_BUTTON 5
 
 Button button(PIN_BUTTON);
 
-class Led 
-{
-public:
-    Led(int r_out, int g_out, int b_out)
-    {
-        R_OUT = r_out;
-        G_OUT = g_out;
-        B_OUT = b_out;
-    }
-    
-    int GetR_OUT() { return R_OUT; }
-    int GetG_OUT() { return G_OUT; }
-    int GetB_OUT() { return B_OUT; }
-private:
-    int R_OUT;
-    int G_OUT;
-    int B_OUT;
-};
-
-int ledsCount = 5;
-int difference = 80;
+int delta = 20;
+int difference = 120;
+int *bluePins = new int[5] { 12, 11, 10, 9, 8 };
 int *X = new int[5] { 255, 255 - difference, 255 - 2 * difference, 255 - 3 * difference, 255 - 4 * difference };
 int *Y = new int[5] { 255, 255 - difference, 255 - 2 * difference, 255 - 3 * difference, 255 - 4 * difference };
-Led *leds = new Led[5] { 
-  Led(9, 10, 11),
-  Led(6, 7, 8),
-  Led(4, 3, 2),
-  Led(37, 39, 41),
-  Led(36, 38, 40) 
-};
-bool waveOn = false;
-int delta = 5;
+int ledsCount = 5;
 
-void set_rgb_led(Led led, int R = 0, int G = 0, int B = 0)
+bool waveOn = false;
+
+void set_B(int pin, int B = 0)
 {
-    analogWrite(led.GetR_OUT(), 255 - R);
-    analogWrite(led.GetG_OUT(), 255 - G);
-    analogWrite(led.GetB_OUT(), 255 - B);
+    analogWrite(pin, 255 - B);
 }
 
 void setup()
 {
     for (int i = 0; i < ledsCount; i++)
     {
-        Led led = leds[i];
-        pinMode(led.GetR_OUT(), OUTPUT);
-        pinMode(led.GetG_OUT(), OUTPUT);
-        pinMode(led.GetB_OUT(), OUTPUT);
+        pinMode(bluePins[i], OUTPUT);
     }
     turnOffAllLeds();
-    Serial.begin(115200);
 }
 
 void loop() 
@@ -82,22 +51,15 @@ void waveIteration()
 {
     for (int i = 0; i < ledsCount; i++)
     {
-        Led led = leds[i];
-        Serial.println(X[i]);
         X[i] += delta;
-        Serial.println(X[i]);
         if (X[i] >= 0 && X[i] <= 255)
             Y[i] = X[i];
         else if (X[i] > 255 && X[i] <= 510)
             Y[i] = 510 - X[i];
         else
             Y[i] = 0;
-        set_rgb_led(led, Y[i], Y[i], Y[i]);
-        /*Serial.print(led.X);
-        Serial.print(" ");
-        Serial.println(led.Y);*/
+        set_B(bluePins[i], Y[i]);
     }
-    Serial.println();
     if (delta > 0 && X[ledsCount - 1] >= 255
       || delta < 0 && X[0] <= 255)
     {
@@ -109,6 +71,6 @@ void turnOffAllLeds()
 {
     for (int i = 0; i < ledsCount; i++)
     {
-        set_rgb_led(leds[i]);
+        set_B(bluePins[i]);
     }
 }
